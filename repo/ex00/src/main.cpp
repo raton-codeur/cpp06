864f519ea7ffae535f8fa207ee278dfc27b6c03c
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-03-31 11:45:57 +0200

    blabla

--- repo/ex00/src/main.cpp
+++ repo/ex00/src/main.cpp
@@ -1,13 +1,18 @@
 #include "ScalarConverter.hpp"
 
+// int main()
+// {
+// 	std::string s;
+// 	while (s != "q")
+// 	{
+// 		std::cout << "literal : ";
+// 		std::getline(std::cin, s);
+// 		ScalarConverter::convert(s);
+// 	}
+// 	return 0;
+// }
+
 int main()
 {
-	std::string s;
-	while (s != "q")
-	{
-		std::cout << "literal : ";
-		std::getline(std::cin, s);
-		ScalarConverter::convert(s);
-	}
-	return 0;
+	std::cout << 5 << std::endl;
 }
---
+++ test/a.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << + 6. << std::endl;
+}
